a77fc138cc740a2f5d528027ec514df3cff138eb
dinesh893686 <dineshshah8234@gmail.com>
2025-02-10 18:59:10 +0530

    solve questions in class

--- Graph/graph.cpp
+++ Graph/graph.cpp
@@ -31,3 +31,40 @@ https://leetcode.com/problems/reorder-routes-to-make-all-paths-lead-to-the-city-
 
 //4. Eventual safe states
 https://www.geeksforgeeks.org/problems/eventual-safe-states/1
+
+
+//5. Longest Cycle in a Graph
+https://leetcode.com/problems/longest-cycle-in-a-graph/description/
+
+
+//6. 
+https://www.geeksforgeeks.org/problems/topological-sort/1
+
+Given an adjacency list for a Directed Acyclic Graph (DAG) where adj[u] contains a list of all vertices v such that there exists a directed edge u to v. Return topological sort for the given graph.
+
+Topological sorting for Directed Acyclic Graph (DAG) is a linear ordering of vertices such that for every directed edge u to v, vertex u comes before v in the ordering.
+Note: As there are multiple Topological orders possible, you may return any of them. If your returned Topological sort is correct then the output will be 1 else 0.
+
+Examples:
+
+Input: adj = [[], [0], [0], [0]] 
+
+Output: 1
+Explanation: The output 1 denotes that the order is valid. Few valid Topological orders for the given graph are:
+[3, 2, 1, 0]
+[1, 2, 3, 0]
+[2, 3, 1, 0]
+
+//7. Course Schedule
+https://leetcode.com/problems/course-schedule-ii/description/
+
+There are a total of numCourses courses you have to take, labeled from 0 to numCourses - 1. You are given an array prerequisites where prerequisites[i] = [ai, bi] indicates that you must take course bi first if you want to take course ai.
+
+For example, the pair [0, 1], indicates that to take course 0 you have to first take course 1.
+Return the ordering of courses you should take to finish all courses. If there are many valid answers, return any of them. If it is impossible to finish all courses, return an empty array.
+
+Example 1:
+
+Input: numCourses = 2, prerequisites = [[1,0]]
+Output: [0,1]
+Explanation: There are a total of 2 courses to take. To take course 1 you should have finished course 0. So the correct course order is [0,1].
